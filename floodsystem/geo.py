2b4916760432dce80edb0d1bb6355d60bd8b58c2
David <dg615@cam.ac.uk>
2022-02-05 16:56:39 +0000

    add function sorting stations by dist

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -7,3 +7,18 @@ geographical data.
 """
 
 from .utils import sorted_by_key  # noqa
+from haversine import haversine
+
+def sort_by_distance(stations, p):
+    """For a list of stations (MonitoringStation object) and
+    coordinate p (latitude, longitude), returns list of tuples
+    (station, distance) sorted by the distance from the given
+    coordinate p"""
+
+    station_dist = []
+
+    for station in stations:
+        station_dist.append((station, haversine(p, station.coord)))
+
+    return sorted_by_key(station_dist, 1)
+
